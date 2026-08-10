93a82cdb146be57473add463b6061ff56ae2b0d1
cdpidan <jks_pidan@foxmail.com>
2026-08-10 15:25:38 +0800

    feat: 更新 HBuilderX 版本至 5.23.2026080626

--- build-docker.sh
+++ build-docker.sh
@@ -1,1 +1,1 @@
-docker buildx build --build-arg HBUILDER_X_VERSION=5.15.2026070915 -t hbuilderx:5.15.2026070915 .
+docker buildx build --build-arg HBUILDER_X_VERSION=5.23.2026080626 -t hbuilderx:5.23.2026080626 .
